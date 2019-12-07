6d9b08f878f506a0ae09bd0a7cd3514820153df9
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-07 21:57:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,10 @@
 class Dog
   attr_accessor :name
   @@all = []
-  @@all_names = []
 
   def initialize(name)
      @name = name
      @@all << self
-     @@all_names << @name
   end
 
   def self.all
